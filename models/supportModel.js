987ff9902e50854d7da8a564ca15b6e6bb699818
jikalvin <pamelafugua@gmail.com>
2024-08-19 09:53:39 -0700

    support

--- models/supportModel.js
+++ models/supportModel.js
@@ -3,12 +3,12 @@ const Schema = mongoose.Schema;
 
 const SupportRequestSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  subject: { type: String, required: true },
+  subject: { type: String },
   message: { type: String, required: true },
   status: { type: String, enum: ['Open', 'Pending', 'Closed'], default: 'Open' },
   responses: [
     {
-      response: { type: String, required: true },
+      response: { type: String },
       responder: { type: Schema.Types.ObjectId, ref: 'Admin' },
       date: { type: Date, default: Date.now }
     }
